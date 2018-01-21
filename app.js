b014f5562da82fd59cde5fbdbb9df629bc6098f7
Domnik Mack <domnikmack@gmail.com>
2018-01-21 10:58:56 -0500

    logging middleware

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+const app = express();
+const logger = morgan('combined');
+
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  res.on('finish', () => console.log(res.statusCode));
+  next();
 });
+
+app.use('/news/', (req, res, next) => {
+  console.log('logging the news route: ' + req.method, req.path);
+  next();
+});
+
+app.use(logger);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/news', (req, res) => res.send('This is the news route'));
 
 app.listen(3000, () => console.log('App listening on port 3000'));
 
